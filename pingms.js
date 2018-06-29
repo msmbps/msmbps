6d5e85daafdb718884e0c2f5b858b80c1a9eb32c
CodeHz <CodeHz@outlook.com>
2018-06-30 00:09:28 +0800

    Improve UX and Optimize code

--- pingms.js
+++ pingms.js
@@ -4,159 +4,258 @@
 // The full project is at https://github.com/pingms/pingms
 //
 //
-ping_start=0;  // The start time of HTTP ping.
-ping_img=null; // The "IMG" object of HTTP ping.
-ping_ok=false; // Whether HTTP ping is done(initial and final).
+"use strict"
+
 //
 //
-// Reset all variables of HTTP ping.
-// Required for starting new HTTP ping.
-function ping_reset()
-{
-    ping_start=0;
-    ping_ok=false;
-    if(ping_img!=null)
-    {
-        ping_img.onerror=null;
-        ping_img.onload=null;
-        ping_img.remove();
-        ping_img=null;
+// Test Data
+const data = {
+    Vultr: [{
+        name: "Frankfurt",
+        url: "http://fra-de-ping.vultr.com/"
+    }, {
+        name: "Amsterdam",
+        url: "http://ams-nl-ping.vultr.com/"
+    }, {
+        name: "Paris",
+        url: "http://par-fr-ping.vultr.com/"
+    }, {
+        name: "London",
+        url: "http://lon-gb-ping.vultr.com/"
+    }, {
+        name: "Singapore",
+        url: "http://sgp-ping.vultr.com/"
+    }, {
+        name: "Tokyo",
+        url: "http://hnd-jp-ping.vultr.com/"
+    }, {
+        name: "New Jersey",
+        url: "http://nj-us-ping.vultr.com/"
+    }, {
+        name: "Chicago",
+        url: "http://il-us-ping.vultr.com/"
+    }, {
+        name: "Seattle",
+        url: "http://wa-us-ping.vultr.com/"
+    }, {
+        name: "Atlanta",
+        url: "http://ga-us-ping.vultr.com/"
+    }, {
+        name: "Miami",
+        url: "http://fl-us-ping.vultr.com/"
+    }, {
+        name: "Dallas",
+        url: "http://tx-us-ping.vultr.com/"
+    }, {
+        name: "Silicon Valley",
+        url: "http://sjo-ca-us-ping.vultr.com/"
+    }, {
+        name: "Los Angeles",
+        url: "http://lax-ca-us-ping.vultr.com/"
+    }, {
+        name: "Sydney",
+        url: "http://syd-au-ping.vultr.com/"
+    }],
+    Linode: [{
+        name: "Newark",
+        url: "http://speedtest.newark.linode.com/"
+    }, {
+        name: "Atlanta",
+        url: "http://speedtest.atlanta.linode.com/"
+    }, {
+        name: "Dallas",
+        url: "http://speedtest.dallas.linode.com/"
+    }, {
+        name: "Fremont",
+        url: "http://speedtest.fremont.linode.com/"
+    }, {
+        name: "Frankfurt",
+        url: "http://speedtest.frankfurt.linode.com/"
+    }, {
+        name: "London",
+        url: "http://speedtest.london.linode.com/"
+    }, {
+        name: "Singapore",
+        url: "http://speedtest.singapore.linode.com/"
+    }, {
+        name: "Tokyo 2",
+        url: "http://speedtest.tokyo2.linode.com/"
+    }],
+    DigitalOcean: [{
+        name: "NYC1",
+        url: "http://speedtest-nyc1.digitalocean.com/"
+    }, {
+        name: "NYC2",
+        url: "http://speedtest-nyc2.digitalocean.com/"
+    }, {
+        name: "NYC3",
+        url: "http://speedtest-nyc3.digitalocean.com/"
+    }, {
+        name: "AMS2",
+        url: "http://speedtest-ams2.digitalocean.com/"
+    }, {
+        name: "AMS3",
+        url: "http://speedtest-ams3.digitalocean.com/"
+    }, {
+        name: "SFO1",
+        url: "http://speedtest-sfo1.digitalocean.com/"
+    }, {
+        name: "SFO2",
+        url: "http://speedtest-sfo2.digitalocean.com/"
+    }, {
+        name: "SGP1",
+        url: "http://speedtest-sgp1.digitalocean.com/"
+    }, {
+        name: "LON1",
+        url: "http://speedtest-lon1.digitalocean.com/"
+    }, {
+        name: "FRA1",
+        url: "http://speedtest-fra1.digitalocean.com/"
+    }, {
+        name: "TOR1",
+        url: "http://speedtest-tor1.digitalocean.com/"
+    }, {
+        name: "BLR1",
+        url: "http://speedtest-blr1.digitalocean.com/"
+    }],
+    VirMach: [{
+        name: "Frankfurt",
+        url: "http://ffm.lg.virmach.com/"
+    }, {
+        name: "Amsterdam",
+        url: "http://ams.lg.virmach.com/"
+    }, {
+        name: "Buffalo",
+        url: "http://ny.lg.virmach.com/"
+    }, {
+        name: "Piscataway (NYC)",
+        url: "http://nj.lg.virmach.com/"
+    }, {
+        name: "Dallas",
+        url: "http://dal.lg.virmach.com/"
+    }, {
+        name: "Phoenix",
+        url: "http://phx.lg.virmach.com/"
+    }, {
+        name: "Los Angeles",
+        url: "http://la.lg.virmach.com/"
+    }, {
+        name: "DDoS-Protected Los Angeles",
+        url: "http://filtered-la.lg.virmach.com/"
+    }, {
+        name: "Chicago",
+        url: "http://chi.lg.virmach.com/"
+    }, {
+        name: "Seattle",
+        url: "http://sea.lg.virmach.com/"
+    }, {
+        name: "Atlanta",
+        url: "http://atl.lg.virmach.com/"
+    }, {
+        name: "San Jose",
+        url: "http://sj.lg.virmach.com/"
+    }]
+};
+
+let nextTick = window.requestAnimationFrame || window.setTimeout // The delay function
+let tasks = [] // Pending task
+let img = document.createElement("img")
+//
+//
+// Prepare to run tests
+function prepare() {
+    const resultArea = document.querySelector("#result");
+    resultArea.appendChild(img);
+    function simpleElement(tag, text) {
+        const el = document.createElement(tag);
+        el.textContent = text;
+        return el;
     }
+    Object.entries(data).forEach(function (pair) {
+        const key = pair[0];
+        const array = pair[1];
+        const subtasks = [];
+        const table = document.createElement("table");
+        table.appendChild(simpleElement("caption", key));
+        const tbody = document.createElement("tbody");
+        array.forEach(function (item) {
+            const bodyline = document.createElement("tr");
+            let result;
+            bodyline.appendChild(simpleElement("td", item.name));
+            bodyline.appendChild(result = simpleElement("td", "Pending..."));
+            result.className = "result";
+            tbody.appendChild(bodyline);
+            subtasks.push({
+                line: bodyline,
+                url: item.url,
+                result: result
+            });
+        });
+        table.appendChild(tbody);
+        resultArea.appendChild(table);
+        tasks.push(subtasks);
+    })
 }
 //
 //
-// Callback function when HTTP ping is done(initial or final).
-// Parameter: time(ms) of HTTP ping.
-function ping_callback(ping_time)
-{
-    if(first_try)
-    {
-        first_try=false;
-        ping(urls[index].innerHTML);
-    }
-    else
-    {
-        results[index].innerHTML=ping_time+"ms";
-        ping_ok=true;
-    }
+// Load image and set callback
+function loadImg(src, callback) {
+    img.src = src + "?res=" + Math.random();
+    img.onerror = callback;
+    img.onload = callback;
 }
 //
 //
-// This function is called when HTTP ping is done.
-// This function is called by "IMG" event.
-function ping_img_done()
-{
-    var ping_end=new Date().getTime();
-    var ping_time=ping_end-ping_start;
-    ping_callback(ping_time);
-}
-//
-//
-// HTTP ping.
-// Parameter: URL such as "http://fra-de-ping.vultr.com/".
-function ping(url)
-{
-    ping_reset();
-    ping_img=document.createElement('img');
-    ping_img.onerror=ping_img_done;
-    ping_img.style.display="none";
-    ping_img.src=url+Math.random()+"?a="+Math.random();
-    ping_start=new Date().getTime();
-    document.body.appendChild(ping_img);
-}
-index=0;                // Current index of URL array.
-first_try=false;        // Whether it's the first try(for caching DNS)
-items=null;             // Array of objects of the "item" class.
-urls=null;              // Array of objects of the "url" class.
-results=null;           // Array of objects of the "result" class.
-sections=[14,22,34,46]; // Index of the last "item" of each cloud provider.
-//
-//
-// Highlight #1 and #2 in results of each cloud provider.
-function best()
-{
-    //alert("best");
-    var range_end=index;
-    var i=sections.indexOf(range_end);
-    if(i==0)
-    {
-        var range_start=0;
-    }
-    else
-    {
-        var range_start=sections[i-1]+1;
-    }
-    var number_one_index=0;
-    var number_one_value=10000;
-    for(var i=range_start;i<=range_end;i++)
-    {
-        var ms_text=results[i].innerHTML;
-        if(ms_text.indexOf("ms")!=-1)
-        {
-            var ms_value=parseInt(ms_text.substring(0,ms_text.indexOf("ms")));
-            if(ms_value<number_one_value)
-            {
-                number_one_value=ms_value;
-                number_one_index=i;
-            }
+// Start Ping
+function handleTasks() {
+    if (tasks.length == 0) return;
+    const currentSubTasks = tasks.shift();
+    const subResults = [];
+    let startTime = 0;
+    function handleSubTasks() {
+        if (currentSubTasks.length == 0) {
+            subResults.sort(function(a, b) {
+                return a.delay - b.delay;
+            });
+            subResults[0].line.className = "first";
+            subResults[1].line.className = "second";
+            subResults[subResults.length - 1].line.className = "last";
+            return nextTick(handleTasks);
         }
-    }
-    items[number_one_index].style.backgroundColor="BlueViolet";
-    var number_two_index=0;
-    var number_two_value=10000;
-    for(var i=range_start;i<=range_end;i++)
-    {
-        var ms_text=results[i].innerHTML;
-        if(ms_text.indexOf("ms")!=-1)
-        {
-            if(i!=number_one_index)
-            {
-                var ms_value=parseInt(ms_text.substring(0,ms_text.indexOf("ms")));
-                if(ms_value<number_two_value)
-                {
-                    number_two_value=ms_value;
-                    number_two_index=i;
+        const task = currentSubTasks.shift();
+        task.result.textContent = "Wait DNS";
+        // First time to load image (skip DNS querying)
+        loadImg(task.url, function() {
+            startTime = new Date().getTime();
+            let finished = false;
+            // Second time to load image (measure delay)
+            nextTick(function rev() {
+                if (!finished) {
+                    const now = new Date().getTime();
+                    task.result.textContent = (now - startTime) + "ms";
+                    nextTick(rev);
                 }
-            }
-        }
+            });
+            loadImg(task.url, function() {
+                finished = true;
+                const now = new Date().getTime();
+                task.result.textContent = (now - startTime) + "ms";
+                subResults.push({
+                    line: task.line,
+                    delay: now - startTime
+                });
+                nextTick(handleSubTasks);
+            });
+        });
     }
-    items[number_two_index].style.backgroundColor="DeepSkyBlue";
-}
-//
-//
-// Run HTTP ping for the next "item".
-function next()
-{
-    if(!ping_ok)
-    {
-        setTimeout(next,25);
-        return;
-    }
-    if(sections.includes(index))
-    {
-        best();
-    }
-    index++;
-    if(index>urls.length-1)
-    {
-        return;
-    }
-    first_try=true;
-    ping(urls[index].innerHTML);
-    setTimeout(next,25);
+    nextTick(handleSubTasks);
 }
 //
 //
 // This function is called by "body onload".
-function main()
-{
-    items=document.getElementsByClassName("item");
-    urls=document.getElementsByClassName("url");
-    results=document.getElementsByClassName("result");
-    index=0;
-    first_try=true;
-    ping(urls[index].innerHTML);
-    setTimeout(next,25);
+function main() {
+    prepare();
+    nextTick(handleTasks);
 }
 //
 //
